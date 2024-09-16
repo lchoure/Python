22b2b37e1fd4f203b383fd87f27196512026e8e5
lchoure <chourelaxman@gmail.com>
2024-09-16 19:39:43 +0200

    file operation

---
+++ fileOperation/fileRead.py
@@ -0,0 +1,38 @@
+# import packages
+import os
+
+#delete files
+
+f = open("C:\\Users\\A9884\\Downloads\\EDIVZ.DRFEHL.SCHQOSA.#240916.txt","r")
+f1 = open("C:\\Users\\A9884\\Downloads\\GF#.txt","w")
+f2 = open("C:\\Users\\A9884\\Downloads\\sp.txt","w")
+
+st1 = 'SCHADEN.GF#='
+st2 = 'N='
+
+cnt = 0
+
+lines = f.readlines()
+
+# get the all Gf#
+for line in lines:
+    if st1 in line:
+        j = line.index(st1)
+        k = len(st1) + 1
+        m = k + 36
+        f1.write("\'{0}\',\n".format(line[k:m]))
+        cnt += 1
+
+# get sparte eigen
+for line in lines:
+    if st2 in line:
+       i = line.index(st2) + 2
+       f2.write(line[i:i+2] + "\n")
+
+print(cnt)
+f1.close()
+f2.close()
+f.close()
+
+
+#print(f.read())
